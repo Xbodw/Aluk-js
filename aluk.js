0b7261bdec4a6f710590a2825d0cb499a9223d21
Xbodw <120997778+Xbodw@users.noreply.github.com>
2023-06-24 19:01:15 +0800

    Update 1.4.2

--- aluk.js
+++ aluk.js
@@ -16,8 +16,8 @@ var aluk = function(select) {
     }
     return new querylist(select);
 };
-aluk.version = '1.2.1';
-aluk.language = 'en-us';
+aluk.version = '1.4.2';
+aluk.language = 'zh-cn';
 
 querylist = function(selector) {
     var ce;
@@ -48,8 +48,10 @@ querylist = function(selector) {
     } else {
         this.push(ce[0]);
     }
+    var fn = this;
+    this.NormalResult = fn[0];
 }
-querylist.prototype = [];
+querylist.prototype = new Array();
 querylist.prototype.createChildElement = function(index,options) {
     if(typeof(options) != 'object') {
         if(aluk.language != 'zh-cn') {
@@ -135,6 +137,41 @@ querylist.prototype.AppendorMoveto = function(index,appender) {
         append = appender;
     }
     append.appendChild(this[index]);
+}
+
+querylist.prototype.RemoveX = function() {
+    let count = 0;
+    this.forEach(s => {
+        s.remove();
+        count++;
+    })
+    return count;
+}
+
+querylist.prototype.continue = function(select) {
+    if(select == undefined || select == '') {
+        if(aluk.language != 'zh-cn') {
+            throw new Error('Your Selector was empty or undefined,please.');
+        } else {
+            throw new Error('您的选择器为空或未定义');
+        }
+    }
+    var newElements = [];
+
+  // 在当前 elements 数组中查找符合选择器的元素，并添加到 newElements 数组中
+  for (var i = 0; i < this.length; i++) {
+    var matches = this[i].querySelectorAll(select);
+    newElements.push(matches);
+  }
+  var newQueryList = new querylist('<null>');
+  newQueryList.shift()
+    newElements.forEach(y => {
+        y.forEach(z => {
+            newQueryList.push(z)
+        })
+    })
+    newQueryList.NormalResult = newQueryList[0];
+  return newQueryList;
 }
 
 aluk.isHtmlElement = function(variable) {
@@ -259,3 +296,38 @@ aluk.htmlToElement = function(html) {
     const doc = parser.parseFromString(html, 'text/html');
     return doc.body.firstChild;
   }
+
+  aluk.WebUrlToBase64 = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+      var reader = new FileReader();
+      reader.onloadend = function() {
+        callback(reader.result);
+      }
+      reader.readAsDataURL(xhr.response);
+    };
+    xhr.open('GET', url);
+    xhr.responseType = 'blob';
+    xhr.send();
+  }
+  
+
+aluk.ShowConsolePictures = function(url,options) {
+    if(typeof(url) != 'string') {
+        throw new Error('请指定URL!')
+    }
+    if(options != undefined && typeof(options) != 'object') {
+        throw new Error('选项必须为Object')
+    }
+    if(document.querySelector('meta[content="no-referrer"]') == null) {
+        document.head.innerHTML += '<meta name="referrer" content="no-referrer">';
+    }
+    var base64Regex = /^data:.*,/;
+    if(base64Regex.test(url) == true) {
+        console.log("%c ", "font-size:" + options.size + "%;line-height:" + options.height + "%;background:url(" + url + ") no-repeat" )
+    } else {
+    aluk.WebUrlToBase64(url, function(base64) {
+        console.log("%c ", "font-size:" + options.size + "%;line-height:" + options.height + "%;background:url(" + base64 + ") no-repeat" )
+      });
+    }
+}
